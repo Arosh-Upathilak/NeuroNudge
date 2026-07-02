25152c34e01e2f08bed4881629670edc7dfa92e3
Aakilahamed006 <ahamedaakil006@gmail.com>
2026-07-02 14:04:24 +0530

    solving merge issued

--- backend/src/routes/nlp.routes.ts
+++ backend/src/routes/nlp.routes.ts
@@ -3,12 +3,12 @@ import { NLPController } from "../controllers/nlp.controller";
 
 import { upload } from "../middleware/upload.middleware";
 import { authMiddleware } from "../middleware/auth.middleware";
-import {mockAuth} from "../mockAuth/mockAuth.middleware";
+
 const router = Router();
-const auth =mockAuth; // Use mockAuth for development/testing
+
 router.post(
   "/chat",
-  auth,
+  authMiddleware,
   upload.single("image"),
   NLPController.processMessage.bind(NLPController)
 );
